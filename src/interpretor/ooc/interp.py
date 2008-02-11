b26de1ae6402d770c7bb6d13ab528962ba69034d
Filia.Tao@gmail.com <Filia.Tao@gmail.com@b75ffedd-2745-0410-9f42-99ac23b86fee>
2008-02-11 03:33:08 +0000

    add ooc package for another language just copy form smalllc

---
+++ src/interpretor/ooc/interp.py
@@ -0,0 +1,458 @@
+#coding=gbk
+'''
+SmallC 语言解释器
+工作在抽象语法树上。
+SmallC 不允许函数嵌套。
+'''
+import operator
+import copy
+import sys
+import interpretor.smallc.lang as lang
+from interpretor.smallc.parse import parse
+from interpretor.smallc.lex import test
+from interpretor.smallc.ast import Node,Leaf
+import interpretor.smallc.error as error
+
+def copy_ns(ns_dict):
+    ret = copy.copy(ns_dict)
+    for x in ret:
+        ret[x] = copy.copy(ns_dict[x])
+    return ret
+
+
+class Namespace:
+    def __init__(self,upper = None):
+        self.upper = upper
+        self.ns = {}
+        self.name = "global"
+
+    def get(self, name):
+        if name in self.ns:
+            return self.ns[name]
+        elif self.upper:
+            return self.upper.get(name)
+        else:
+            #print self.ns
+            raise error.NameError(name)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def set(self, name, value):
+        #print self.name,id(self.ns)
+        if name in self.ns:
+            raise error.MultipleError(name)
+        else:
+            self.ns[name] = value
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
+    def __repr__(self):
+        return "Namespace %s" %(self.name)
+
+class Function(Namespace):
+    def __init__(self,name,upper,ret_type = lang.void):
+        self.name = name
+        self.upper = upper
+        self.ns = {}
+        self.ret_type = ret_type
+        self.params = []
+        self.statements = []
+
+    def add_param(self,name,type):
+        self.params.append(name)
+        self.set(name,lang.Object(type))
+
+    def freeze(self):
+        self.ns_org = copy_ns(self.ns)
+
+    def set_param(self, name, value):
+        if name not in self.ns:
+            raise NameError(name)
+        else:
+            self.ns[name].op("assign",value)
+
+    def set(self, name, value):
+        if name in self.ns:
+            raise MultipleError(name)
+        else:
+            self.ns[name] = value
+
+    def call(self,args,inter):
+        ns_now = self.ns
+        self.ns = copy_ns(self.ns_org)
+
+        old_current = inter.current_ns
+        inter.current_ns = self
+
+        for i in range(len(self.params)):
+            self.set_param(self.params[i], args[i])
+        for st in self.statements:
+            ret = inter.on_statement(st)
+        self.ns = ns_now
+        inter.current_ns = old_current
+        return ret.op("tcast", self.ret_type)
+
+    def __repr__(self):
+        return "Function %s " %self.name
+
+class PrintFunc(Function):
+    def __init__(self):
+        self.name = "print"
+
+    def call(self,args,inter):
+        for x in args:
+            x.op("print")
+        return lang.Object(lang.void)
+
+    def __repr__(self):
+        return "function %s" %(self.name)
+
+class PrintlnFunc(Function):
+    def __init__(self):
+        self.name = "println"
+
+    def call(self,args,inter):
+        for x in args:
+            x.op("print")
+        print
+        return lang.Object(lang.void)
+    def __repr__(self):
+        return "function %s" %(self.name)
+
+
+inputFlags = {
+    "InputBuff" : "",
+    "isEOF" : 0
+}
+class ReadFunc(Function):
+    def __init__(self,input):
+        self.name = "read"
+        self.input = input
+    def call(self,args,inter):
+        if self.input["InputBuff"]:
+            inp = self.input["InputBuff"]
+            try:
+                self.input["InputBuff"] = ""
+                self.input["InputBuff"] = raw_input()
+            except EOFError,e:
+                self.input["isEOF"] = 1
+        else:
+            inp = raw_input()
+        return lang.Object(lang.intType, int(inp))
+    def __repr__(self):
+        return "function %s" %(self.name)
+
+class EofFunc(Function):
+    def __init__(self,input):
+        self.name = "eof"
+        self.input = input
+    def call(self,args,inter):
+        if not self.input["InputBuff"] and not self.input["isEOF"]:
+            try:
+                self.input["InputBuff"] = raw_input()
+            except EOFError,e:
+                self.input["isEOF"] = 1
+        return lang.Object(lang.intType, self.input["isEOF"])
+    def __repr__(self):
+        return "function %s" %(self.name)
+
+def get_built_in_namespace():
+    ns = Namespace()
+    ns.ns = {
+        'int':lang.intType,
+        'void':lang.void,
+        'null':lang.null,
+        'print':PrintFunc(),
+        'println':PrintlnFunc(),
+        'read':ReadFunc(inputFlags),
+        'eof':EofFunc(inputFlags)
+    }
+    return ns
+
+
+class MoreParser:
+    '''在AST 基础上进一步处理，根据声明语句建立名字空间和函数对象'''
+    def __init__(self,ast):
+        self.ast = ast
+        self.global_ns = get_built_in_namespace()
+        self.current_ns = self.global_ns
+
+    def parse(self):
+        '''walk the ast , build the golbal namespace'''
+
+        #类定义
+        for n in self.ast.query("class_decls>classdecl"):
+            name = n.child(1).value
+            struct = lang.Struct(name)
+            self.global_ns.set(name, struct)
+
+
+        for n in self.ast.query("class_decls>classdecl"):
+            name = n.child(1).value
+            struct = self.global_ns.get(name)
+            for x in n.child(3):
+                self.on_decl_inside_class(x,struct)
+
+        #常量
+        for n in self.ast.query("condecl>condef"):
+            self.on_condef(n,self.global_ns)
+
+        #变量
+        for decl in self.ast.query("vdecl>decllist>decl"):
+            self.on_decl(decl,self.global_ns)
+
+        #函数
+        for n in self.ast.query("fdefs>fdef"):
+            self.on_fdef(n,self.global_ns)
+
+    def on_decl(self,node,ns):
+        type = self.on_type(node.child(0))
+        for id in node.child(1):
+            ns.set(id.value,lang.Object(type))
+
+    def on_decl_inside_class(self,node,struct):
+        type = self.on_type(node.child(0))
+        for id in node.child(1):
+            struct.add_member(type,id.value)
+
+    #函数形参定义
+    def on_paradecl(self,node,ns):
+        type = self.on_type(node.child(0))
+        name = node.child(1).value
+        ns.add_param(name,type)
+
+    def on_type(self,node):
+        base = node.child(0).value
+        base_type = self.current_ns.get(base)
+        if not base_type:
+            pass # raise Error
+        else:
+            if len(node) > 1:
+                dim = (len(node) - 1)/2
+                return lang.Array(base_type, dim)
+            else:
+                return base_type
+
+    def on_condef(self,node,ns):
+        #print node
+        name = node.child(0).value
+        value = node.child(-1).value
+        if len(node) > 3:
+            value = -value
+        ns.set(name,lang.ConstObject(lang.intType,value)) # type use lang.intType
+
+    def on_fdef(self,node,ns):
+        name  = node.child(2).child(0).value
+        fns = Function(name,self.current_ns)
+        fns.ret_type = self.on_type(node.child(1))
+        ns.set(name,fns)
+
+        for para in node.query("head>paralist>paradecl"):
+            self.on_paradecl(para,fns)
+        for decl in node.query("funbody>vdecl>decllist>decl"):#vdecl > decllist > decls
+            self.on_decl(decl,fns)
+        fns.statements = node.query("funbody>stlist>st")
+        fns.freeze()
+
+class Interpreter:
+
+    def __init__(self,ast,global_ns):
+        self.ast = ast
+        self.global_ns = global_ns
+        self.current_ns = None
+
+    def run(self):
+        self.current_ns = self.global_ns
+        try:
+            self.current_ns.get("main").call([],self)
+        except error.LangError,e:
+            print "error at line %d near token '%s': %s" %(self.current_token.lineno,self.current_token.value,str(e))
+
+
+    def on_statement(self,node):
+        node = node.child(0)
+        if node.type == "cond":
+            return self.on_cond(node)
+        elif node.type == "loop":
+            return self.on_loop(node)
+        elif node.type == "exp":
+            return self.on_exp(node)
+
+    def on_cond(self,node):
+        #print node
+        exp = node.child(2)
+        st = node.child(4)
+        if self.on_exp(exp):
+            return self.on_statement(st)
+        elif len(node) > 6:
+            return self.on_statement(node.child(6))
+        return lang.Object(lang.void)
+
+    def on_loop(self,node):
+        #print node
+        exp = node.child(2)
+        ret = lang.Object(lang.void)
+        while self.on_exp(exp):
+            if len(node) > 4:
+                ret = self.on_statement(node.child(4))
+        return ret
+
+    def on_exp(self,node):
+        #print node
+        if len(node) > 1:
+            lhs = self.on_orexp(node.child(0))
+            self.on_token(node.child(1))
+            rhs = self.on_orexp(node.child(2))
+            return lhs.op("assign",rhs)
+        else:
+            return self.on_orexp(node.child(0))
+
+    def on_orexp(self,node):
+        if len(node) > 1:
+            lhs = self.on_orexp(node.child(0))
+            self.on_token(node.child(1))
+            rhs = self.on_andexp(node.child(2))
+            return lhs.op("or",rhs)
+        else:
+            return self.on_andexp(node.child(0))
+
+    def on_andexp(self,node):
+        if len(node) > 1:
+            lhs = self.on_andexp(node.child(0))
+            self.on_token(node.child(1))
+            rhs = self.on_relexp(node.child(2))
+            return lhs.op("and",rhs)
+        else:
+            return self.on_relexp(node.child(0))
+
+    def on_relexp(self,node):
+        if len(node) > 1:
+            lhs = self.on_relexp(node.child(0))
+            m = {
+             '==':'eq',
+             '!=':'ne',
+             '<':'lt',
+             '>':'gt',
+             '<=':'le',
+             '>=':'ge'
+            }
+            relop = m[self.on_token(node.child(1))]
+            rhs = self.on_term(node.child(2))
+
+            return lhs.op(relop,rhs)
+        else:
+            return self.on_term(node.child(0))
+
+    def on_term(self,node):
+        if len(node) > 1:
+            lhs = self.on_term(node.child(0))
+            op = {'+':'add','-':'minus'}[self.on_token(node.child(1).child(0))]
+            rhs = self.on_factor(node.child(2))
+            return lhs.op(op,rhs)
+        else:
+            return self.on_factor(node.child(0))
+
+    def on_factor(self,node):
+        if len(node) > 1:
+            lhs = self.on_factor(node.child(0))
+            op =  {'*':'mul','/':'div','%':'mod'}[self.on_token(node.child(1).child(0))]
+            rhs = self.on_uniexp(node.child(2))
+            return lhs.op(op,rhs)
+        else:
+            return self.on_uniexp(node.child(0))
+
+    def on_uniexp(self,node):
+        if len(node) > 1:
+            uniop = {'++':'inc','--':'dec',
+                    '-':'minus_','!':'not','chk':'chk'}[self.on_token(node.child(0).child(0))]
+            uniexp = self.on_uniexp(node.child(1))
+            return uniexp.op(uniop)
+        else:
+            return self.on_postexp(node.child(0))
+
+    def on_postexp(self,node):
+
+        if len(node) > 1:
+            postexp = self.on_postexp(node.child(0))
+            postfix = node.child(1).child(0)
+            if postfix.type == 'apara':
+                if len(postfix) == 2:
+                    return postexp.call([],self)
+                else:
+                    return postexp.call(self.on_apara(postfix),self)
+            elif postfix.type =='sub':
+                return postexp.op("index",self.on_exp(postfix.child(1)))
+            elif postfix.type == 'aselect':
+                return postexp.op("member",self.on_token(postfix.child(1)))
+            elif postfix.type == 'tcast':
+                return postexp.op("tcast",self.on_type(postfix.child(1)))
+            if isinstance(postfix,Leaf):
+                value = self.on_token(postfix)
+                if value == '++':
+                    return postexp.op("inc_")
+                elif value == '--':
+                    return postexp.op("dec_")
+        else:
+            return self.on_entity(node.child(0))
+
+
+
+    def on_type(self,node):
+        base = self.on_token(node.child(0))
+        base_type = self.current_ns.get(base)
+        if len(node) > 1:
+            dim = len(node) - 1
+            return lang.Array(base_type, dim)
+        else:
+            return base_type
+
+    def on_entity(self,node):
+        entity = node.child(0)
+        if entity.type == "cast":
+            return self.on_cast(entity)
+        elif entity.type == "alloc":
+            return self.on_alloc(entity)
+        elif isinstance(entity,Leaf):
+            entity = self.on_token(entity)
+            if isinstance(entity,str):
+                if entity == '?': #input
+                    return self.current_ns.get("read").call([],self)
+                else:
+                    return self.current_ns.get(entity)
+            elif isinstance(entity,int):
+                return lang.Object(lang.intType, entity)
+
+    def on_cast(self,node):
+        '''cast 的语义？ 最后一个statement 的值'''
+        for x in node.query("stlist>st"):
+            ret = self.on_statement(x)
+        return ret
+
+    def on_alloc(self,node):
+        if len(node) == 2:
+            ret =  self.on_type(node.child(1)).alloc()
+        else:
+            ret = self.on_type(node.child(1)).alloc(self.on_exp(node.child(3)))
+        return ret
+
+    def on_apara(self,node):
+        return [self.on_exp(x) for x in node.query("explist>exp")]
+
+    def on_token(self,node):
+        self.current_token = node
+        return node.value
+
+
+def run():
+    ast = parse(test)
+    parser = MoreParser(ast)
+    parser.parse()
+    #print parser.global_ns.ns
+    inter = Interpreter(ast,parser.global_ns)
+    inter.run()
+    #print inter.global_ns.ns
+
+if __name__ == '__main__':
+    run()
---
+++ src/interpretor/ooc/parse.py
@@ -0,0 +1,418 @@
+#coding=gbk
+from ply import yacc
+
+from interpretor.smallc.lex import *
+from interpretor.smallc.ast import Node,Leaf
+
+#def sall_to_nodee(func):
+#    def wrapped(p):
+#        for i in range(len(p)):
+#            if p[i] and not isinstance(p[i], Node):
+#                p[i] = Leaf(child, p.lineno(i), p.lexpos(i))
+#        return func(p)
+#    wrapped.__doc__ = func.__doc__
+#
+#    return wrapped
+
+def all_to_node(p):
+    for i in range(len(p)):
+        if p[i] is not None and not isinstance(p[i], Node):
+            p[i] = Leaf(p[i], p.lineno(i), p.lexpos(i))
+
+start = 'prog'
+def p_empty(p):
+    "empty : "
+    pass
+
+#程序
+def p_prog(p):
+    '''prog : class_decls const_decls var_decls fdefs
+    '''
+    p[0] = Node("prog",p[1:])
+
+#类声明s
+def p_class_decls(p):
+    '''class_decls : class_decls classdecl
+                   | classdecl
+                   | empty
+    '''
+    if len(p) > 2 :
+        if p[1]:
+            p[0] = Node("class_decls", p[1].getChildren() + [p[2]])
+        else:
+            p[0] = Node("class_decls", [p[2]])
+    elif p[1]:
+        p[0] = Node("class_decls",[p[1]])
+
+
+
+
+def p_classdecl(p):
+    "classdecl : kw_class id '{' decllist '}'"
+    all_to_node(p)
+    p[0] = Node("classdecl",p[1:])
+
+
+def p_decllist(p):
+    '''decllist : decl ';' decllist
+                | decl
+    '''
+    all_to_node(p)
+    if len(p) > 2:
+        p[0] = Node("decllist",[p[1]] + p[3].getChildren())
+    else:
+        p[0] = Node("decllist",p[1:])
+
+def p_decl(p):
+    "decl : type idlist"
+    p[0] = Node("decl",p[1:])
+
+#类型
+
+def p_type(p):
+    '''type : type '[' ']'
+            | id
+    '''
+    all_to_node(p)
+    #the final ast should like  id {'[' ']'}
+    if len(p) > 2:
+        p[0] = Node("type",p[1].getChildren() + p[2:])
+    else:
+        p[0] = Node("type",p[1:])
+
+
+def p_idlist(p):
+    '''idlist : id ',' idlist
+              | id
+    '''
+    all_to_node(p)
+    if len(p) > 2:
+        p[0] = Node("idlist",[p[1]] + p[3].getChildren())
+    else:
+        p[0] = Node("idlist",p[1:])
+
+
+#可能的常量声明
+
+def p_const_decls(p):
+    '''const_decls : condecl ';'
+                   | empty
+    '''
+    all_to_node(p)
+    p[0] = p[1]
+
+
+def p_condecl(p):
+    '''condecl : condecl ',' condef
+               | kw_const condef
+    '''
+    all_to_node(p)
+    if len(p) > 3:
+        p[0] = Node("condecl",p[1].getChildren() + p[3:])
+    else:
+        p[0] = Node("condecl",p[1:])
+
+#常量定义
+
+def p_condef(p):
+    '''condef : id '=' num
+              | id '=' '-' num
+    '''
+    all_to_node(p)
+    p[0] = Node("condef",p[1:])
+
+
+#变量声明
+def p_var_decls(p):
+    '''var_decls : vdecl
+                 | empty
+    '''
+    p[0] = p[1]
+
+
+def p_vdecl(p):
+    "vdecl : kw_var decllist kw_end"
+    all_to_node(p)
+    p[0] = Node("vdecl",p[1:])
+
+
+#函数定义s
+def p_fdefs(p):
+    ''' fdefs : fdef fdefs
+              | fdef
+    '''
+    if len(p) > 2:
+        p[0] = Node("fdefs",[p[1]] + p[2].getChildren())
+    else:
+        p[0] = Node("fdefs",p[1:])
+
+
+def p_fdef(p):
+    "fdef : kw_func type head '{' funbody '}'"
+    all_to_node(p)
+    p[0] = Node("fdef", p[1:])
+
+
+
+def p_head(p):
+    '''head : id '(' ')'
+            | id '(' paralist ')'
+    '''
+    all_to_node(p)
+    p[0] = Node("head", p[1:])
+
+
+def p_paralist(p):
+    '''paralist : paradecl
+                | paradecl ',' paralist
+    '''
+    all_to_node(p)
+    if len(p) > 2:
+        p[0] = Node("paralist",[p[1]] + p[3].getChildren())
+    else:
+        p[0] = Node("paralist",p[1:])
+
+
+def p_paradecl(p):
+    "paradecl : type id "
+    all_to_node(p)
+    p[0] = Node("paradecl", p[1:])
+
+
+def p_funbody(p):
+    '''funbody : vdecl stlist
+                | stlist
+    '''
+    p[0] = Node("funbody", p[1:])
+
+
+
+def p_stlist(p):
+    '''stlist : st ';' stlist
+              | st
+    '''
+    all_to_node(p)
+    if len(p) > 2:
+        p[0] = Node("stlist",[p[1]] + p[3].getChildren())
+    else:
+        p[0] = Node("stlist",p[1:])
+
+def p_st(p):
+    '''st : exp
+          | cond
+          | loop
+    '''
+    p[0] = Node("st",p[1:])
+
+
+
+def p_cond(p):
+    '''cond : kw_if '(' exp ')' st
+            | kw_if '(' exp ')' st  kw_else st
+    '''
+    all_to_node(p)
+    p[0] = Node("cond",p[1:])
+
+
+def p_loop(p):
+    '''loop : kw_while '(' exp ')'
+            | kw_while '(' exp ')' st
+    '''
+    all_to_node(p)
+    p[0] = Node("loop",p[1:])
+
+
+def p_exp(p):
+    '''exp : orexp
+           | orexp '=' orexp
+    '''
+    all_to_node(p)
+    p[0] = Node("exp",p[1:])
+
+
+
+def p_orexp(p):
+    '''orexp : andexp
+             | orexp  orop andexp
+    '''
+    all_to_node(p)
+    p[0] = Node("orexp",p[1:])
+
+
+def p_andexp(p):
+    '''andexp : relexp
+              | andexp andop relexp
+    '''
+    all_to_node(p)
+    p[0] = Node("andexp",p[1:])
+
+
+def p_relexp(p):
+    '''relexp : term
+              | relexp relop term
+    '''
+    all_to_node(p)
+    p[0] = Node("relexp",p[1:])
+
+
+def p_relop(p):
+    '''relop : eqop
+             | neop
+             | ltop
+             | gtop
+             | leop
+             | geop
+    '''
+    all_to_node(p)
+    p[0] = p[1]
+
+
+def p_term(p):
+    '''term : factor
+            | term addop factor
+    '''
+    p[0] = Node("term",p[1:])
+
+
+def p_addop(p):
+    '''addop : '+'
+             | '-'
+    '''
+    all_to_node(p)
+    p[0] = Node("addop",p[1:])
+
+
+def p_factor(p):
+    '''factor : uniexp
+              | factor multop uniexp
+    '''
+    p[0] = Node("factor",p[1:])
+
+
+def p_mulop(p):
+    '''multop : '*'
+              | '/'
+              | '%'
+    '''
+    all_to_node(p)
+    p[0] = Node("multop",p[1:])
+
+
+
+def p_uniexp(p):
+    '''uniexp : uniop uniexp
+              | postexp
+    '''
+    p[0] = Node("uniexp",p[1:])
+
+
+def p_uniop(p):
+    '''uniop : '-'
+             | '!'
+             | incop
+             | decop
+             | chkop
+    '''
+    all_to_node(p)
+    p[0] = Node("uniop",p[1:])
+
+
+def p_postexp(p):
+    '''postexp : entity
+               | postexp postfix
+    '''
+    p[0] = Node("postexp",p[1:])
+
+
+def p_postfix(p):
+    '''postfix : incop
+               | decop
+               | apara
+               | sub
+               | aselect
+               | tcast
+    '''
+    all_to_node(p)
+    p[0] = Node("postfix",p[1:])
+
+
+def p_apara(p):
+    '''apara : '(' explist ')'
+             | '(' ')'
+    '''
+    all_to_node(p)
+    p[0] = Node("apara",p[1:])
+
+
+def p_explist(p):
+    '''explist : exp
+               | exp ',' explist
+    '''
+    all_to_node(p)
+    if len(p) > 2:
+        p[0] = Node("explist",[p[1]] + p[3].getChildren())
+    else:
+        p[0] = Node("explist",p[1:])
+
+
+def p_sub(p):
+    "sub : '[' exp ']'"
+    all_to_node(p)
+    p[0] = Node("sub",p[1:])
+
+
+def p_aselect(p):
+    "aselect : '.' id"
+    all_to_node(p)
+    p[0] = Node("aselect",p[1:])
+
+
+def p_tcast(p):
+    "tcast : ':' type"
+    all_to_node(p)
+    p[0] = Node("tcast",p[1:])
+
+
+def p_entity(p):
+    '''entity : id
+              | num
+              | cast
+              | alloc
+              | '?'
+    '''
+    all_to_node(p)
+    p[0] = Node("entity",p[1:])
+
+
+def p_cast(p):
+    "cast : '(' stlist ')'"
+    all_to_node(p)
+    p[0] = Node("cast",p[1:])
+
+
+def p_alloc(p):
+    '''alloc : kw_new type
+             | kw_new type '[' exp ']'
+    '''
+    all_to_node(p)
+    p[0] = Node("alloc",p[1:])
+
+
+def p_error(p):
+    print p , "at line " , p.lineno
+
+parser = yacc.yacc()
+
+def parse(data):
+    parser.error = 0
+    parser.functions = {}
+    #p = parser.parse(data)
+    p = parser.parse(data,debug=1)
+    if parser.error: return None
+    p.functions = parser.functions
+    return p
+
+if __name__ == '__main__':
+    n =  parse(test)
+    print len(n.query("vdecl>decllist>decl"))
+    print len(n.query("class_decls>classdecl"))
