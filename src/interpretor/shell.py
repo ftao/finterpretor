0d4237f6ac2b1725bea3b6cefd16e04fb9c45102
Filia.Tao@gmail.com <Filia.Tao@gmail.com@b75ffedd-2745-0410-9f42-99ac23b86fee>
2008-02-21 04:05:12 +0000

    basic shell finished

--- src/interpretor/shell.py
+++ src/interpretor/shell.py
@@ -13,9 +13,10 @@ class Shell:
         self.code = ""
         self.engineer = engineer
         self.name = "L"
-
+        self.recent_command = []
+        self.version = "0.1"
     def start(self):
-        print self.name , "shell 0.1"
+        print self.name , "shell ", self.version
         print "type help for help"
         self.run()
 
@@ -25,53 +26,85 @@ class Shell:
             try:
                 command = raw_input(">>>")
             except EOFError,e:
+                print "Please use 'exit' to exit the shell"
                 continue
-            if command.startswith("clear"):
-                self.clear()
-            elif command.startswith("read"):
-                self.read()
-            elif command.startswith("load"):
-                filename = eval(command.split()[1])
-                self.load(filename)
-            elif  command.startswith("exit"):
-                self.exit()
-            elif command.startswith("help"):
-                self.help()
-            elif command == "":
-                pass
-            else:
-                print "error command"
+            if command:
+                self.recent_command.append(command)
+                self.on_command(command)
 
 
-
+    def on_command(self,command):
+        if command.startswith("clear"):
+            self.clear()
+        elif command.startswith("read"):
+            self.read()
+        elif command.startswith("load"):
+            try:
+                filename = command.split()[1]
+                if filename[0] == '"' and filename[-1] == '"':
+                    filename = filename[1:-1]
+            except (IndexError),e:
+                print "bad argument for load command"
+                return
+            self.load(filename)
+        elif  command.startswith("exit"):
+            self.exit()
+        elif command.startswith("help"):
+            self.help()
+        elif command.startswith("list"):
+            for i in range(len(self.recent_command)):
+                print "%d : %s" %(i+1, self.recent_command[i])
+            print "using exec <number> to exec recent command"
+        elif command.startswith("exec"):
+            try:
+                number = int(command.split()[1])
+            except (IndexError,ValueError),e:
+                print "bad argument for exec command"
+                return
+            try:
+                self.on_command(self.recent_command[number-1])
+            except IndexError,e:
+                print "No command with id %d" %(number,)
+        else:
+            print "error command"
     def clear(self):
         self.code = ""
 
     def read(self):
         self.clear()
-        try:
-            while(True):
-                self.code += raw_input() + '\n'
-        except EOFError,e:
-            pass
-        print self.code
-        self.engineer.run(self.code)
+        while(True):
+            try:
+                self.code += raw_input()
+            except EOFError,e:
+                break
+        if self.code:
+            self.engineer.run(self.code)
 
     def exit(self):
         sys.exit()
 
     def help(self):
+        print self.name , "shell ", self.version
+        print "Author: TaoFei (Filia.Tao@gmail.com)"
         print "supported commands:"
-        print "  read : read from stdin and run the code (using CTRL+Z(Windows) or CTRL+D(*nix)"
-        print "  load : load from file and run the code "
+        print "  clear : clear"
         print "  exit : eixt"
-        print "  clear: clear"
+        print "  exec <command-id>: execute recent command"
         print "  help : show this message"
+        print "  list : list recent commands"
+        print "  load <filename> : load from file and run the code "
+        print "  read : read from stdin and run the code"
+        print "         using CTRL+Z+Return(Windows) or CTRL+D(*nix) to finish the input"
+
+
 
     def load(self,filename):
-        self.code = open(filename).read()
+        try:
+            self.code = open(filename).read()
+        except IOError,e:
+            print "cann't load file '%s'" %(filename)
+            return
         self.engineer.run(self.code)
-        self.clear()
 
 if __name__ == '__main__':
     import  interpretor.smallc.interp as engieer
