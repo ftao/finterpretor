408493cad8ba603782cc43480aec774bc379ad69
Filia.Tao@gmail.com <Filia.Tao@gmail.com@b75ffedd-2745-0410-9f42-99ac23b86fee>
2008-03-09 08:35:19 +0000

    add test smallc test ran

---
+++ src/test/smallctest.py
@@ -0,0 +1,43 @@
+#coding=gbk
+'''Unit Test For interpretor.smallc package'''
+import unittest
+import sys
+import os
+import glob
+import StringIO
+import interpretor.smallc.interp as interp
+
+
+
+def run_and_get_output(code,infile):
+    outp = StringIO.StringIO()
+    interp.run(code, infile, outp)
+    return outp.getvalue()
+
+def infile_to_outfile(infile):
+    return infile[:infile.rfind('.')] + '.out'
+
+class GoodSourceTest(unittest.TestCase):
+    def test_all(self):
+        print "runing "
+        print "get test files from folder 'smallc'"
+        prefix = ''
+        for src_file in glob.glob('./smallc/*.src'):
+            if os.path.isfile(src_file):
+                code = open(src_file).read()
+                input_file = open(src_file[:src_file.rfind('.')] + '.in')
+                expect_out = open(src_file[:src_file.rfind('.')] + '.out').read()
+                print "Current File : %s" %(src_file,)
+                out = run_and_get_output(code,input_file)
+                self.assertEqual(out,expect_out)
+                print "OK"
+
+
+
+class BadSourceTest(unittest.TestCase):
+    pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
