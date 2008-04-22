69c2cae5457f5bd39021ad45fa3ac81c8709fc19
Filia.Tao@gmail.com <Filia.Tao@gmail.com@b75ffedd-2745-0410-9f42-99ac23b86fee>
2008-04-22 13:28:20 +0000

    astwalker generator

--- trunk/src/interpretor/smallc/sementic.py
+++ trunk/src/interpretor/smallc/sementic.py
@@ -35,79 +35,131 @@ def check_type_requirement(op_name, *operands):
 
 #这个文件定义了语义动作
 
-class BaseSementicAction:
+class BaseASTWalker:
 
-    def on_node(self, node):
-        pass
+    def on_prog(self, node):
+        return node.child(0)
 
-    def on_decl(self,node,ns):
-        pass
+    def on_class_decls(self, node):
+        return node.child(0)
 
-    def on_decl_inside_class(self,node,struct):
-        pass
+    def on_classdecl(self, node):
+        return node.child(0)
 
-    def on_paradecl(self,node,ns):
-        pass
+    def on_decllist(self, node):
+        return node.child(0)
 
-    def on_type(self,node):
-        pass
+    def on_decl(self, node):
+        return node.child(0)
 
-    def on_condef(self,node,ns):
-        pass
+    def on_type(self, node):
+        return node.child(0)
 
-    def on_fdef(self,node,ns):
-        pass
+    def on_idlist(self, node):
+        return node.child(0)
 
-    def on_statement(self, *node):
-        return node
+    def on_condecl(self, node):
+        return node.child(0)
 
-    def on_cond(self, *node):
-        pass
+    def on_condef(self, node):
+        return node.child(0)
 
-    def on_loop(self,node):
-        pass
+    def on_vdecl(self, node):
+        return node.child(0)
 
-    def on_exp(self,node):
-        pass
+    def on_fdefs(self, node):
+        return node.child(0)
 
-    def on_orexp(self,node):
-        pass
+    def on_fdef(self, node):
+        return node.child(0)
 
-    def on_andexp(self,node):
-        pass
+    def on_head(self, node):
+        return node.child(0)
 
-    def on_relexp(self,node):
-        pass
+    def on_paralist(self, node):
+        return node.child(0)
 
-    def on_term(self,node):
-        pass
+    def on_paradecl(self, node):
+        return node.child(0)
 
-    def on_factor(self,node):
-        pass
+    def on_funbody(self, node):
+        return node.child(0)
 
-    def on_uniexp(self,node):
-        pass
+    def on_stlist(self, node):
+        return node.child(0)
 
-    def on_postexp(self,node):
-        pass
+    def on_st(self, node):
+        return node.child(0)
 
-    def on_type(self,node):
-        pass
+    def on_cond(self, node):
+        return node.child(0)
 
-    def on_entity(self,node):
-        pass
+    def on_loop(self, node):
+        return node.child(0)
 
-    def on_cast(self,node):
-        pass
+    def on_exp(self, node):
+        return node.child(0)
 
-    def on_alloc(self,node):
-        pass
+    def on_orexp(self, node):
+        return node.child(0)
 
-    def on_apara(self,node):
-        pass
+    def on_andexp(self, node):
+        return node.child(0)
 
-    def on_token(self,node):
-        pass
+    def on_relexp(self, node):
+        return node.child(0)
+
+    def on_relop(self, node):
+        return node.child(0)
+
+    def on_term(self, node):
+        return node.child(0)
+
+    def on_addop(self, node):
+        return node.child(0)
+
+    def on_factor(self, node):
+        return node.child(0)
+
+    def on_mulop(self, node):
+        return node.child(0)
+
+    def on_uniexp(self, node):
+        return node.child(0)
+
+    def on_uniop(self, node):
+        return node.child(0)
+
+    def on_postexp(self, node):
+        return node.child(0)
+
+    def on_postfix(self, node):
+        return node.child(0)
+
+    def on_apara(self, node):
+        return node.child(0)
+
+    def on_explist(self, node):
+        return node.child(0)
+
+    def on_sub(self, node):
+        return node.child(0)
+
+    def on_aselect(self, node):
+        return node.child(0)
+
+    def on_tcast(self, node):
+        return node.child(0)
+
+    def on_entity(self, node):
+        return node.child(0)
+
+    def on_cast(self, node):
+        return node.child(0)
+
+    def on_alloc(self, node):
+        return node.child(0)
+
 
 
 
