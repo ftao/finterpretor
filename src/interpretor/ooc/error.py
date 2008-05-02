6d6b85893822ec6177bdb4f88fdb89702a2aedf8
Filia.Tao@gmail.com <Filia.Tao@gmail.com@b75ffedd-2745-0410-9f42-99ac23b86fee>
2008-05-02 04:59:46 +0000

    ooc static sementantic check

--- src/interpretor/ooc/error.py
+++ src/interpretor/ooc/error.py
@@ -1,8 +1,59 @@
 #coding=utf8
 #$Id$
 
+class Error(Exception):
+
+    error_type = "error" #can be error, warning , notice
+
+    def __init__(self, lineno, msg):
+        self.lineno = lineno
+        self.msg = msg
+
+    def __str__(self):
+        return "line %s: %s: %s" %(self.lineno, self.error_type, self.msg)
+
 class LangError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+    __repr__ = __str__
+
+#静态语义错误
+class StaticSemanticError(LangError):
+    '''静态语义错误'''
     pass
+
+
+class NameError(StaticSemanticError):
+    def __init__(self, name):
+        self.name = name
+        self.msg = "name '%s' is not defined" %(self.name)
+
+class NameReDefineError(StaticSemanticError):
+    def __init__(self, name):
+        self.name = name
+        self.msg = "name '%s' is already defined" %(self.name)
+
+class TypeCheckError(StaticSemanticError):
+    def __init__(self, op):
+        self.op = op
+        self.msg = "type not match for operation '%s'" %(self.op)
+
+class MemberError(StaticSemanticError):
+    def __init__(self, type, member):
+        self.type = type
+        self.member = member
+        self.msg = "'%s' dont't have '%s' member" %(self.type.name, self.member)
+
+class ParamCountNotMatchError(StaticSemanticError):
+    def __init__(self, expect_count, real_count):
+        self.expect_count = expect_count
+        self.real_count = real_count
+        self.msg = "param count not match , expect %d , got %d" %(self.expect_count, self.real_count)
+
 
 class TCastError(LangError):
 
@@ -22,13 +73,6 @@ class TypeError(LangError):
     def __str__(self):
         return "Except %s , got %s" %(self.lhs,self.rhs)
 
-
-class NameError(LangError):
-    def __init__(self,name):
-        self.name = name
-
-    def __str__(self):
-        return "Undefined name %s" %(self.name)
 
 class MultipleError(LangError):
     def __init__(self,name):
