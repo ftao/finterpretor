8e609f47f64edbfb6850cd1473e17aa6bea415e9
Filia.Tao@gmail.com <Filia.Tao@gmail.com@b75ffedd-2745-0410-9f42-99ac23b86fee>
2008-05-29 12:45:54 +0000

    rewrite L1 interpretor using abstract syntax way.

--- src/interpretor/smallc/interp.py
+++ src/interpretor/smallc/interp.py
@@ -153,27 +153,26 @@ class Interpreter:
                         print >>sys.stderr, "call %s at line %s" %(x[0], x[1])
                     else:
                         print >>sys.stderr, "call %s" % (x[0])
-        except StandardError,e:
-            print >>sys.stderr, "Interpretor inner error "
-            raise
+        #except StandardError,e:
+        #    print >>sys.stderr, "Interpretor inner error "
+        #    raise
 
     def on_node(self, node):
-        if isinstance(node, Node):
+        if isinstance(node, Leaf):
+            return self.on_token(node)
+        else:
             if hasattr(self, 'on_' + node.type):
                 return getattr(self, 'on_' + node.type)(node)
             else:
-                pass #should we report error here ?
-        else:
-            return self.on_token(node)
+                if len(node) == 1:
+                    return self.on_node(node.child(0))
+                else:
+                    print >>sys.stderr, "not such node ", node.type, node
 
-    def on_statement(self,node):
-        node = node.child(0)
-        if node.type == "cond":
-            return self.on_cond(node)
-        elif node.type == "loop":
-            return self.on_loop(node)
-        elif node.type == "exp":
-            return self.on_exp(node)
+
+
+    def on_statement(self, node):
+        return self.on_node(node.child(0))
 
     def on_cond(self,node):
         #print node
@@ -195,115 +194,68 @@ class Interpreter:
         return ret
 
     def on_exp(self,node):
-        #print node
-        if len(node) > 1:
-            lhs = self.on_orexp(node.child(0))
-            self.on_token(node.child(1))
-            rhs = self.on_orexp(node.child(2))
-            return lhs.op("assign",rhs)
-        else:
-            return self.on_orexp(node.child(0))
+        return self.on_node(node.child(0))
+
+
+
+    def on_binexp(self, node):
+        lhs = self.on_node(node.child(0))
+        self.on_node(node.child(1))
+        op_name = node.child(1).get_attr('op_name')
+        rhs = self.on_node(node.child(2))
+        return lhs.op(op_name, rhs)
+
+    on_assignexp = on_binexp
 
     def on_orexp(self,node):
-        if len(node) > 1:
-            lhs = self.on_orexp(node.child(0))
-            if lhs:
-                return lhs
-            self.on_token(node.child(1))
-            rhs = self.on_andexp(node.child(2))
-            return lhs.op("or",rhs)
-        else:
-            return self.on_andexp(node.child(0))
+        lhs = self.on_node(node.child(0))
+        if lhs:
+            return lhs
+        self.on_node(node.child(1))
+        rhs = self.on_node(node.child(2))
+        return lhs.op("or",rhs)
 
     def on_andexp(self,node):
-        if len(node) > 1:
-            lhs = self.on_andexp(node.child(0))
-            if not lhs:
-                return lhs
-            self.on_token(node.child(1))
-            rhs = self.on_relexp(node.child(2))
-            return lhs.op("and",rhs)
-        else:
-            return self.on_relexp(node.child(0))
-
-    def on_relexp(self,node):
-        if len(node) > 1:
-            lhs = self.on_relexp(node.child(0))
-            m = {
-             '==':'eq',
-             '!=':'ne',
-             '<':'lt',
-             '>':'gt',
-             '<=':'le',
-             '>=':'ge'
-            }
-            relop = m[self.on_token(node.child(1).child(0))]
-            rhs = self.on_term(node.child(2))
-
-            return lhs.op(relop,rhs)
-        else:
-            return self.on_term(node.child(0))
-
-    def on_term(self,node):
-        if len(node) > 1:
-            lhs = self.on_term(node.child(0))
-            op = {'+':'add','-':'minus'}[self.on_token(node.child(1).child(0))]
-            rhs = self.on_factor(node.child(2))
-            return lhs.op(op,rhs)
-        else:
-            return self.on_factor(node.child(0))
-
-    def on_factor(self,node):
-        if len(node) > 1:
-            lhs = self.on_factor(node.child(0))
-            op =  {'*':'mul','/':'div','%':'mod'}[self.on_token(node.child(1).child(0))]
-            rhs = self.on_uniexp(node.child(2))
-            return lhs.op(op,rhs)
-        else:
-            return self.on_uniexp(node.child(0))
+        lhs = self.on_node(node.child(0))
+        if not lhs:
+            return lhs
+        self.on_node(node.child(1))
+        rhs = self.on_node(node.child(2))
+        return lhs.op("and",rhs)
 
     def on_uniexp(self,node):
-        if len(node) > 1:
-            uniop = {'++':'inc','--':'dec',
-                    '-':'minus_','!':'not','chk':'chk'}[self.on_token(node.child(0).child(0))]
-            uniexp = self.on_uniexp(node.child(1))
-            return uniexp.op(uniop)
-        else:
-            return self.on_postexp(node.child(0))
+        op_name = node.child(0).get_attr('op_name')
+        uniexp = self.on_node(node.child(1))
+        return uniexp.op(op_name)
 
     def on_postexp(self,node):
-
-        if len(node) > 1:
-            postexp = self.on_postexp(node.child(0))
-            postfix = node.child(1).child(0)
-            if postfix.type == 'apara':
-                line_no = self.current_token.lineno
-                ret = None
-                if len(postfix) == 2:
-                    ret = postexp.call([],self,line_no)
-                else:
-                    ret = postexp.call(self.on_apara(postfix),self,line_no)
-                self.on_token(postfix.child(-1))# read the ')', to set the current_token right
-                return ret
-            elif postfix.type =='sub':
-                return postexp.op("index",self.on_exp(postfix.child(1)))
-            elif postfix.type == 'aselect':
-                return postexp.op("member",self.on_token(postfix.child(1)))
-            elif postfix.type == 'tcast':
-                return postexp.op("tcast",self.on_type(postfix.child(1)))
-            if isinstance(postfix,Leaf):
-                value = self.on_token(postfix)
-                if value == '++':
-                    return postexp.op("inc_")
-                elif value == '--':
-                    return postexp.op("dec_")
-        else:
-            return self.on_entity(node.child(0))
-
+        postexp = self.on_node(node.child(0))
+        postfix = node.child(1).child(0)
+        if postfix.type == 'apara':
+            line_no = self.current_token.lineno
+            ret = None
+            if len(postfix) == 2:
+                ret = postexp.call([],self,line_no)
+            else:
+                ret = postexp.call(self.on_node(postfix),self,line_no)
+            self.on_node(postfix.child(-1))# read the ')', to set the current_token right
+            return ret
+        elif postfix.type =='sub':
+            return postexp.op("index",self.on_node(postfix.child(1)))
+        elif postfix.type == 'aselect':
+            return postexp.op("member",self.on_node(postfix.child(1)))
+        elif postfix.type == 'tcast':
+            return postexp.op("tcast",self.on_node(postfix.child(1)))
+        if isinstance(postfix,Leaf):
+            value = self.on_node(postfix)
+            if value == '++':
+                return postexp.op("inc_")
+            elif value == '--':
+                return postexp.op("dec_")
 
 
     def on_type(self,node):
-        base = self.on_token(node.child(0))
+        base = self.on_node(node.child(0))
         base_type = self.current_ns.get(base)
         if len(node) > 1:
             dim = (len(node) - 1)/2
@@ -314,11 +266,11 @@ class Interpreter:
     def on_entity(self,node):
         entity = node.child(0)
         if entity.type == "cast":
-            return self.on_cast(entity)
+            return self.on_node(entity)
         elif entity.type == "alloc":
-            return self.on_alloc(entity)
+            return self.on_node(entity)
         elif isinstance(entity,Leaf):
-            entity = self.on_token(entity)
+            entity = self.on_node(entity)
             if isinstance(entity,str):
                 if entity == '?': #input
                     return self.current_ns.get("read").call([],self)
@@ -330,229 +282,22 @@ class Interpreter:
     def on_cast(self,node):
         '''cast 的语义？ 最后一个statement 的值'''
         for x in node.query("stlist>st"):
-            ret = self.on_statement(x)
+            ret = self.on_node(x)
         return ret
 
     def on_alloc(self,node):
         if len(node) == 2:
-            ret =  self.on_type(node.child(1)).alloc()
+            ret =  self.on_node(node.child(1)).alloc()
         else:
-            ret = self.on_type(node.child(1)).alloc(self.on_exp(node.child(3)))
+            ret = self.on_node(node.child(1)).alloc(self.on_node(node.child(3)))
         return ret
 
     def on_apara(self,node):
-        return [self.on_exp(x) for x in node.query("explist>exp")]
+        return [self.on_node(x) for x in node.query("explist>exp")]
 
     def on_token(self,node):
         self.current_token = node
         return node.value
-
-class Interpreter2(BaseASTWalker, BaseAnnotateAction):
-    '''递归解释器(这个版块不可用)'''
-
-    annotate_attr_name = "RUNTIME_VALUE"
-
-    def __init__(self,ast,global_ns):
-        self.ast = ast
-        self.global_ns = global_ns
-        self.current_ns = None
-        self.current_token = None
-        self.call_stack = []
-        self.action = self
-
-    def run(self):
-        self.current_ns = self.global_ns
-        try:
-            self.current_ns.get("main").call([],self)
-        except error.LangError,e:
-            if self.current_token is None:
-                print >>sys.stderr,e
-            else:
-                print >>sys.stderr, "error at line %d near token '%s': %s" %(self.current_token.lineno,self.current_token.value,str(e))
-                print >>sys.stderr, "calling stack "
-                for x in self.call_stack:
-                    if x[1]:
-                        print >>sys.stderr, "call %s at line %s" %(x[0], x[1])
-                    else:
-                        print >>sys.stderr, "call %s" % (x[0])
-        #except AssertionError,e:
-        #    print "error at line " , self.current_token.lineno
-        #    print e
-        #except StandardError,e:
-        #    print >>sys.stderr, "Interpretor inner error "
-        #    raise e
-
-
-    on_st = BaseAnnotateAction._copy_from_first_child
-
-
-    def walk_cond(self, node):
-        self._walk_node(node.child(2))
-        if self.action.get_node_attr(node.child(2)):
-            self._walk_node(node.child(4))
-        elif len(node) > 6:
-            self._walk_node(node.child(6))
-        self._do_action(node)
-
-    def on_cond(self, node):
-         #总是返回lang.Object(lang.Void)
-        self.set_node_attr(node, lang.Object(lang.void))
-
-    def walk_loop(self, node):
-        while True:
-            self._walk_node(node.child(2))
-            if not self.action.get_node_attr(node.child(2)):
-                break
-            if len(node) > 4:
-                self._walk_node(node.child(4))
-        return self._do_action(node)
-
-    def on_loop(self, node):
-        self.set_node_attr(node, lang.Object(lang.void))
-
-    def _on_bin_exp(self, node):
-        lhs = self.get_node_attr(node.child(0))
-        assert lhs is not None
-        if len(node) > 1:
-
-            op_name = node.child(1).get_attr('op_name')
-            rhs = self.get_node_attr(node.child(2))
-            v = lhs.op(op_name, rhs)
-            assert v is not None
-            self.set_node_attr(node, v)
-        else:
-            self._copy_from_first_child(node)
-        #print "after_", node.type, node, node._attr
-
-    on_exp = _on_bin_exp
-
-    def walk_orexp(self, node):
-        self._walk_node(node.child(0))
-        if len(node) > 1 and not self.action.get_node_attr(node.child(0)):
-            self._walk_node(node.child(1))
-            self._walk_node(node.child(2))
-        self._do_action(node)
-
-    def on_orexp(self,node):
-        #print "on_orexp", node
-        lhs = self.get_node_attr(node.child(0))
-        assert lhs is not None
-        if lhs:
-            self.set_node_attr(node, lhs)
-        else:
-            self._on_bin_exp(node)
-        #print "on_" , node.type , node, node._attr
-
-    def walk_andexp(self, node):
-        self._walk_node(node.child(0))
-        if len(node) > 1 and self.action.get_node_attr(node.child(0)):
-            self._walk_node(node.child(1))
-            self._walk_node(node.child(2))
-        self._do_action(node)
-
-    def on_andexp(self, node):
-        lhs = self.get_node_attr(node.child(0))
-        assert lhs is not None
-        if not lhs:
-            self.set_node_attr(node, lhs)
-        else:
-            self._on_bin_exp(node)
-        #print "on_" , node.type , node, node._attr
-
-    on_relexp = on_term = on_factor = _on_bin_exp
-
-
-
-    def on_uniexp(self, node):
-        if len(node) > 1:
-            op_name = node.child(0).get_attr('op_name')
-            obj = self.get_node_attr(node.child(1))
-            self.set_node_attr(node, obj.op(op_name))
-        else:
-            self._copy_from_first_child(node)
-        #print "uniexp" , node, node._attr
-
-    def on_postexp(self,node):
-        if len(node) > 1:
-            postexp = self.get_node_attr(node.child(0))
-            postfix = node.child(1).child(0)
-            if postfix.type == 'apara':
-                #print "function call ", postexp
-                line_no = self.current_token.lineno
-                ret = postexp.call(self.get_node_attr(postfix),self,line_no)
-
-                self.set_node_attr(node, ret)
-            elif postfix.type =='sub':
-                #print "index is ", postfix
-                self.set_node_attr(node, postexp.op("index", self.get_node_attr(postfix.child(1))))
-            elif postfix.type == 'aselect':
-                self.set_node_attr(node, postexp.op("member",postfix.child(1).value))
-            elif postfix.type == 'tcast':
-                self.set_node_attr(node, postexp.op("tcast", self.get_node_attr(postfix.child(1))))
-            elif isinstance(postfix,Leaf):
-                op_name = postfix.get_attr('op_name')
-                self.set_node_attr(node, postexp.op(op_name))
-            else:
-                assert False
-        else:
-            self._copy_from_first_child(node)
-
-        #print "postexp" , node, node._attr
-
-    def on_type(self,node):
-        base_type = self.current_ns.get(node.child(0).value)
-        if len(node) > 1:
-            dim = (len(node) - 1)/2
-            self.set_node_attr(node, lang.Array(base_type, dim))
-        else:
-            self.set_node_attr(node, base_type)
-
-    def on_entity(self, node):
-        self._copy_from_first_child(node)
-        #print "entity" , node, node._attr, node.child(0).type
-        assert self.get_node_attr(node) is not None
-
-    def on_cast(self, node):
-        '''cast 的语义？ 最后一个statement 的值'''
-        #print "quering st " , node.query('stlist')[0]._attr
-        self.set_node_attr(node, self.get_node_attr(node.child('stlist')))
-        #print "after cast " ,node, node._attr
-        assert self.get_node_attr(node) is not None
-
-    def on_stlist(self, node):
-         #print "on_stlist is running " , node
-         self.set_node_attr(node, self.get_node_attr(node.query('st')[-1]))
-         #print "stlist", node._attr
-         assert self.get_node_attr(node) is not None
-
-    def on_alloc(self,node):
-        if len(node) == 2:
-            self.set_node_attr(node, self.get_node_attr(node.child(1)).alloc())
-        else:
-            self.set_node_attr(node, self.get_node_attr(node.child(1)).alloc(self.get_node_attr(node.child(3))))
-
-    def on_apara(self,node):
-        self.set_node_attr(node, [self.get_node_attr(x) for x in node.query("explist>exp")])
-
-
-    def _on_token(self,node):
-        #print "on_token " , node
-        if node.type == 'num':
-            self.set_node_attr(node, lang.Object(lang.intType, node.value))
-        elif node.type == '?':
-            self.set_node_attr(node, self.current_ns.get("read").call([],self))
-        elif node.type == 'id':
-            v = self.current_ns.get(node.value)
-            self.set_node_attr(node, v)
-#            if isinstance(v, lang.Object):
-#                self.set_node_attr(node, v)
-#                print node._attr
-#            elif isinstance(v, Function):
-#                self.set_node_attr(node, v)
-        else:
-            pass
-            #print "something else " , node
-        self.current_token = node
 
 class StaticTypeChecker(BaseAnnotateAction):
     '''静态类型检查和计算'''
@@ -586,7 +331,7 @@ class StaticTypeChecker(BaseAnnotateAction):
         return is_type_match
 
 
-    def _on_bin_exp(self, node):
+    def on_binexp(self, node):
         if len(node) >1:
             self._do_type_trans(node,
                 node.child(1).get_attr('op_name'),
@@ -605,7 +350,6 @@ class StaticTypeChecker(BaseAnnotateAction):
     def on_funbody(self, node):
         self.current_ns = self.global_ns
 
-
     on_st = BaseAnnotateAction._copy_from_first_child
 
     def on_cond(self, node):
@@ -614,9 +358,7 @@ class StaticTypeChecker(BaseAnnotateAction):
     def on_loop(self, node):
         node.set_attr(self.annotate_attr_name, lang.void)
 
-    on_exp = on_orexp = on_andexp = _on_bin_exp
-
-    on_relexp = on_term = on_factor = _on_bin_exp
+    on_exp = on_orexp = on_binexp
 
 
     def on_uniexp(self, node):
